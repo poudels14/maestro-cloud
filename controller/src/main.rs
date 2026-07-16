8d554d6653ba055c4de1af2c70e1ef0873f18b9f
Sagar Poudel <poudels14@gmail.com>
2026-07-15 17:29:20 -0700

    upgrade to 0.3.3

--- controller/src/main.rs
+++ controller/src/main.rs
@@ -287,7 +287,7 @@ enum ClusterCommand {
     },
     /// Upgrade system components
     #[command(
-        after_help = "Examples:\n  maestro cluster upgrade\n  maestro cluster upgrade --version 0.3.2\n  maestro cluster upgrade system"
+        after_help = "Examples:\n  maestro cluster upgrade\n  maestro cluster upgrade --version 0.3.3\n  maestro cluster upgrade system"
     )]
     Upgrade {
         #[command(subcommand)]
